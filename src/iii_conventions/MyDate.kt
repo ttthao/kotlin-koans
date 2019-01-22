12c6061b245408b0a331d0af13567a67e13f15e9
Tommy Truong <ttthao@ucsd.edu>
2019-01-21 18:16:18 -0800

    Fix iii_conventions

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,18 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override operator fun compareTo(other: MyDate): Int = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+}
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other);
+
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = addTimeIntervals(timeInterval, 1);
+operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval): MyDate = addTimeIntervals(repeatedTimeInterval.timeInterval, repeatedTimeInterval.count);
+
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +20,32 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+operator fun TimeInterval.times(count: Int) = RepeatedTimeInterval(this, count);
+
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>, Iterable<MyDate> {
+    override operator fun contains(d: MyDate): Boolean = (d >= start) &&  (d <= endInclusive);
+    override operator fun iterator(): Iterator<MyDate> = DateIterator(this);
+}
+
+class DateIterator(val dateRange: DateRange) : Iterator<MyDate> {
+    var current: MyDate = dateRange.start;
+
+    // Return current, set current to next day
+    override operator fun next(): MyDate {
+        val result: MyDate = current
+        current = current.nextDay();
+        return result;
+    }
+
+    override operator fun hasNext(): Boolean = current <= dateRange.endInclusive;
+
+}
+
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val count: Int);
+
+
+// My solution for ClosedRange
+//class DateRange(val start: MyDate, val endInclusive: MyDate)
+//operator fun DateRange.contains(d: MyDate): Boolean {
+//    return (d >= start) && (d <= endInclusive);
+//}
--- src/iii_conventions/n31Invoke.kt
+++ src/iii_conventions/n31Invoke.kt
@@ -3,7 +3,14 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable(private var count: Int = 0) {
+    operator fun invoke(): Invokable {
+        count++
+        return this
+    };
+
+    fun getNumberOfInvocations(): Int = count;
+}
 
 fun todoTask31(): Nothing = TODO(
     """
@@ -14,6 +21,5 @@ fun todoTask31(): Nothing = TODO(
     references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }
